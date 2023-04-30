ff0259fbaeefa18a92aa10a1fea7e40b6405603f
2Ruk <leerukdev@gmail.com>
2023-04-30 22:16:47 +0900

    refactor: common provider init

--- server/libs/share-library/src/config/config.ts
+++ server/libs/share-library/src/config/config.ts
@@ -2,6 +2,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
 import { QuestionEntity } from '@src/balance/question/entities/question.entity';
 import { AnswerEntity } from '@src/balance/answer/entities/answer.entity';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
+import { HttpExceptionFilter } from '@app/share-library/filter/http-exception.filter';
+import { LoggingInterceptor } from '@app/share-library/interceptors/logging.interceptor';
 
 export const configSetting = () => {
   const isProduction = process.env.NODE_ENV === 'production';
@@ -32,3 +36,33 @@ export const dbSetting = async (
     poolSize: 10,
   };
 };
+
+export const AppGuard = () => {
+  const guardList = [ThrottlerGuard];
+  return guardList.map((guard) => ({
+    provide: APP_GUARD,
+    useClass: guard,
+  }));
+};
+
+export const AppFilter = () => {
+  const filterList = [HttpExceptionFilter];
+  return filterList.map((filter) => ({
+    provide: APP_FILTER,
+    useClass: filter,
+  }));
+};
+export const AppInterceptor = () => {
+  const interceptorList = [LoggingInterceptor];
+
+  return interceptorList.map((interceptor) => ({
+    provide: APP_INTERCEPTOR,
+    useClass: interceptor,
+  }));
+};
+
+export const AppCommonProvider = [
+  ...AppGuard(),
+  ...AppFilter(),
+  ...AppInterceptor(),
+];
--- server/libs/share-library/src/share-library.module.ts
+++ server/libs/share-library/src/share-library.module.ts
@@ -3,6 +3,8 @@ import { ShareLibraryService } from './share-library.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configSetting, dbSetting } from '@app/share-library/config/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -12,6 +14,13 @@ import { configSetting, dbSetting } from '@app/share-library/config/config';
       inject: [ConfigService],
       useFactory: dbSetting,
     }),
+    // cache manger v5 에서는 ttl 이 milliseconds 단위로 변경됨
+    CacheModule.register({
+      ttl: 3000,
+      max: 10,
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot({ ttl: 1, limit: 60 }),
   ],
   providers: [ShareLibraryService],
   exports: [ShareLibraryService],
