86f52b0f76c60da8d1e743721e9498370dfb86c6
2Ruk <leerukdev@gmail.com>
2023-05-01 18:47:53 +0900

    docs: og tag add

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>IT 밸런스 게임</title>
+        <meta property="og:title" content="IT 밸런스 게임" />
+        <meta property="og:description" content="개발자 밸런스 게임!" />
+        <meta property="og:url" content="https://app.itquiz.co.kr/" />
+        <meta
+          property="og:image"
+          content="https://app.itquiz.co.kr/common/dev-balance.png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
