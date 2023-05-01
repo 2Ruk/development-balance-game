7307eff4683be67058c0ce8f18b1cf1e95749c7e
2Ruk <leerukdev@gmail.com>
2023-05-01 19:00:36 +0900

    refactor: og tag fix

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -7,22 +7,25 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>IT 밸런스 게임</title>
-        <meta property="og:title" content="IT 밸런스 게임" />
-        <meta property="og:description" content="개발자 밸런스 게임!" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@isValidUserId" />
+        <meta name="twitter:creator" content="@isValidUserId" />
+        <meta name="twitter:title" content="IT 밸런스 게임" />
+        <meta name="twitter:description" content="개발자 밸런스 게임!" />
+        <meta
+          name="twitter:image"
+          content="https://app.itquiz.co.kr/dev-balance.png"
+        />
 
         <meta property="og:url" content="https://app.itquiz.co.kr/" />
+        <meta property="og:title" content="IT 밸런스 게임" />
+        <meta property="og:description" content="개발자 밸런스 게임!" />
         <meta
           property="og:image"
           content="https://app.itquiz.co.kr/dev-balance.png"
         />
-        <meta
-          name="twitter:card"
-          content="https://app.itquiz.co.kr/dev-balance.png"
-        />
-        <meta name="twitter:site" content="https://app.itquiz.co.kr/" />
-        <meta name="twitter:creator" content="@isValidUserId" />
-        <meta name="twitter:title" content="IT 밸런스 게임" />
-        <meta name="twitter:description" content="개발자 밸런스 게임!" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
       </Head>
       <Component {...pageProps} />
     </>
