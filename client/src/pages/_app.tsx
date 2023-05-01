a46e8c69ecceaa8c21fa88d5d442244d91f6ae3e
2Ruk <leerukdev@gmail.com>
2023-05-01 18:49:16 +0900

    docs: og tag add

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>IT 밸런스 게임</title>
+        <meta property="og:title" content="IT 밸런스 게임" />
+        <meta property="og:description" content="개발자 밸런스 게임!" />
+        <meta property="og:url" content="https://app.itquiz.co.kr/" />
+        <meta
+          property="og:image"
+          content="https://app.itquiz.co.kr/common/dev-balance.png"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -3,16 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>IT 밸런스 게임</title>
-        <meta property="og:title" content="IT 밸런스 게임" />
-        <meta property="og:description" content="개발자 밸런스 게임!" />
-        <meta property="og:url" content="https://app.itquiz.co.kr/" />
-        <meta
-          property="og:image"
-          content="https://app.itquiz.co.kr/common/dev-balance.png"
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
